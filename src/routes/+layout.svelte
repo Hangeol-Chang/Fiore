d616bf494cfda789983dbc99e01938f72279bb18
Hangeol-Chang <hgchang1@naver.com>
2026-03-28 13:23:10 +0900

    hotfix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
-	import { Analytics } from '@vercel/analytics/svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	
+	inject({ mode: dev ? 'development' : 'production' });
+
 	import '@/styles/globals.scss';
 	import Header from '@/components/layout/header.svelte';
 	import Footer from '@/components/layout/footer.svelte';
@@ -20,8 +24,6 @@
 	{@render children()}
 </div>
 <Footer />
-
-<Analytics />
 
 <style>
 	.content {
